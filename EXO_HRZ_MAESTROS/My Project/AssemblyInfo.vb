1eb103f938c7b257df5c9c6ba3a90d20e2b20b7b
Oscar J. Martínez <omartinez@expertone.es>
2023-04-26 16:15:30 +0200

    UPDATE

--- EXO_HRZ_MAESTROS/My Project/AssemblyInfo.vb
+++ EXO_HRZ_MAESTROS/My Project/AssemblyInfo.vb
@@ -12,7 +12,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("")>
 <Assembly: AssemblyCompany("EXPERT ONE")>
 <Assembly: AssemblyProduct("EXO_HRZ_MAESTROS")>
-<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyCopyright("Copyright ©  2023")>
 <Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' mediante el carácter '*', como se muestra a continuación:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("10.1.8.20")>
-<Assembly: AssemblyFileVersion("10.1.8.20")>
+<Assembly: AssemblyVersion("10.1.8.23")>
+<Assembly: AssemblyFileVersion("10.1.8.23")>
